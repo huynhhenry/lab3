47e99fc6957bd336a019db936425a601d0bffb93
Henry Huynh <146884910+huynhhenry@users.noreply.github.com>
2023-11-07 12:13:15 -0800

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,24 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
-
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversedWithBug() {
+    int[] input = { 1, 2, 3 };
+    assertArrayEquals(new int[]{ 3, 2, 1 }, ArrayExamples.reversed(input));
+}
+    
+  @Test
+  public void testAverageWithoutLowest() {
+      double[] input = { 3.0, 1.0, 2.0, 4.0, 5.0 };
+      double expected = 3.5;
+      
+      double result = ArrayExamples.averageWithoutLowest(input);
+      assertEquals(expected, result, 0.001);
   }
 }
